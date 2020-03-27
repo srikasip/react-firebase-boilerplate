23594d6542fdfeb20e6203a80bb8c56969158719
Srikar Prasad <srikasip@gmail.com>
2020-03-27 17:42:20 -0400

    removing sensitive data

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,12 +1,3 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyBQQgGOB-y3kk35oDAdVEI3QNG0dLYc688",
-  authDomain: "boilerplate-bcc48.firebaseapp.com",
-  databaseURL: "https://boilerplate-bcc48.firebaseio.com",
-  projectId: "boilerplate-bcc48",
-  storageBucket: "boilerplate-bcc48.appspot.com",
-  messagingSenderId: "258518144988",
-  appId: "1:258518144988:web:3a0a3af5a008006ed5a119",
-  measurementId: "G-HFNY0GWM05"
-};
+var firebaseConfig = {};
 
 export default firebaseConfig;
